a0f2bd33adea5315e9b1c5e0eb910841145aa2cc
Isaac J <jimenez.isaac.jr@gmail.com>
2024-04-30 16:29:03 -0700

    add missing styles

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 import HomePage from "./HomePage";
 import { View} from 'react-native';
+import { styles } from '../assets/css';
 
 export default function Page() {
     return (
-        <View >
+        <View style={styles.center} >
             <HomePage />
         </View>
     );
